db023006c8842a7ab97b41689f0d88a93120657b
Yuri U <y.unterreiner@outlook.com>
2020-10-29 18:12:01 -0500

    Create Purchasing Agent List

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,10 @@
 import {BusinessList} from "./BusinessList.js"
 import {NYList} from "./NYList.js"
 import {mList} from "./manufacturingList.js"
+import {AgentList} from "./AgentList.js"
 
-BusinessList();
-NYList();
-mList();
+
+BusinessList()
+NYList()
+mList()
+AgentList()
